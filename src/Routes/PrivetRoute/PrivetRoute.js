e7820e3cf48079fca4b8c2e8de2f5372b40a6824
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-11-26 09:52:32 +0600

    create privet route

---
+++ src/Routes/PrivetRoute/PrivetRoute.js
@@ -0,0 +1,18 @@
+import { React, useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../contexts/Authprovider/AuthProvider';
+import Loading from '../../Pages/Shared/Loading/Loading'
+
+const PrivetRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const location = useLocation()
+    if (loading) {
+        return <Loading />
+    }
+    if (user) {
+        return children;
+    }
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+};
+
+export default PrivetRoute;
